20d6bb4e5e7c2a8ecf6c320c0e943a4555492805
Jorge-Montes <985747889pedrohuainalaya@gmail.com>
2026-05-28 00:55:42 -0500

    Se agrega el metodo para listar candidatos ordenados por apellido

--- sistema-votacion/src/main/java/com/votacion/sistema_votacion/repository/CandidatoRepository.java
+++ sistema-votacion/src/main/java/com/votacion/sistema_votacion/repository/CandidatoRepository.java
@@ -4,6 +4,10 @@ import com.votacion.sistema_votacion.model.Candidato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface CandidatoRepository extends JpaRepository<Candidato, Long>{
     
+    //Listar todos los candidatos por apellido
+    List<Candidato> findAllByOrderByApellidoAsc();
 }
