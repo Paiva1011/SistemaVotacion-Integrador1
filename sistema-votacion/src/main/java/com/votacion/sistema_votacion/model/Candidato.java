74ffb7312e3f68f8799b1744079fcfe27e989a9b
Jorge-Montes <985747889pedrohuainalaya@gmail.com>
2026-05-28 00:15:39 -0500

    Se agrego las anotaciones JPA a la entidad Candidato

--- sistema-votacion/src/main/java/com/votacion/sistema_votacion/model/Candidato.java
+++ sistema-votacion/src/main/java/com/votacion/sistema_votacion/model/Candidato.java
@@ -1,15 +1,28 @@
 package com.votacion.sistema_votacion.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "candidatos")
+
 public class Candidato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
 
+    @Column(nullable = false)
     private String apellido;
 
+    @Column(nullable = false)
     private String partido;
 
+    @Column(columnDefinition = "TEXT")
     private String propuesta;
 
+    @Column(nullable = false)
     private int totalVotos = 0;
 }
