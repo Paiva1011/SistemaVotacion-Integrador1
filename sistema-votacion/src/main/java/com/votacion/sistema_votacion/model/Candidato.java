2c8562f3d32e5c320d6ba6bba2e29c4ed2c33ffe
Jorge-Montes <985747889pedrohuainalaya@gmail.com>
2026-05-28 00:03:15 -0500

    Se crea la clase Candidato y sus atributos de la tabla.

---
+++ sistema-votacion/src/main/java/com/votacion/sistema_votacion/model/Candidato.java
@@ -0,0 +1,15 @@
+package com.votacion.sistema_votacion.model;
+
+public class Candidato {
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String partido;
+
+    private String propuesta;
+
+    private int totalVotos = 0;
+}
