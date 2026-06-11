7bdb5d912294c05fd8989f42491081588242f6ff
Luciana Canales <U23304661@utp.edu.pe>
2026-06-11 07:39:45 -0500

    docs: comentarios en VotanteController

--- sistema-votacion/src/main/java/com/votacion/sistema_votacion/controller/VotanteController.java
+++ sistema-votacion/src/main/java/com/votacion/sistema_votacion/controller/VotanteController.java
@@ -3,9 +3,18 @@ package com.votacion.sistema_votacion.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controlador encargado de gestionar las vistas relacionadas
+ * con el registro de votantes.
+ */
 @Controller
 public class VotanteController {
 
+    /**
+     * Muestra el formulario de registro de votantes.
+     *
+     * @return Nombre de la vista que contiene el formulario de registro.
+     */
     @GetMapping("/votantes/registro")
     public String mostrarFormulario() {
         return "registro";
